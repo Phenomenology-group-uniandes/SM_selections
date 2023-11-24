64a2c95365ef033c9ee889e484539cab1338d146
cfrc2694 <crisfer2694@hotmail.com>
2023-11-24 09:13:19 -0500

    ADD MET Particle

---
+++ src/Atlas_Reader_13TeV/particles/met_particle.py
@@ -0,0 +1,17 @@
+from ROOT import TLorentzVector
+
+from hep_pheno_tools.abstract_particle import Particle
+
+
+class MetParticle(Particle):
+    def __init__(self, event):
+        super().__init__()
+        self.tlv.SetPtEtaPhiE(
+            event.met_et / 1000,
+            0,
+            event.met_phi,
+            event.met_et / 1000,
+        )
+        self.charge = 0.0
+        self.name = "MET"
+        self.kind = "MET"
