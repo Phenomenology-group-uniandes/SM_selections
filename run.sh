0e0cb138398f44e5b50be98eaa94925e3c058511
cfrc2694 <crisfer2694@hotmail.com>
2023-12-15 13:13:42 -0500

    ADD symbolic link to the rawdatafolder

--- run.sh
+++ run.sh
@@ -62,6 +62,9 @@ docker build -t sm_selections .
 mkdir -p "$OUTPUT_PATH"
 echo "Raw Data folder: $OUTPUT_PATH"
 
+# Create a symbolic link to the output folder
+ln -sf "$OUTPUT_PATH" OutputFolder
+
 # Run the container, use 10 cores i,e 20 threads
 docker run -t \
   --env-file .env \
